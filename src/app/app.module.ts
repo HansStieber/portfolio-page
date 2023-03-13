36f72e29d8e2e0ee24c5ddb8c8c024067212fe28
HansStieber <hans.stieber@posteo.de>
2023-03-13 17:57:36 +0100

    continued work at start-screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { PortfolioPageComponent } from './portfolio-page/portfolio-page.component';
+import { StartScreenComponent } from './start-screen/start-screen.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
-    PortfolioPageComponent
+    PortfolioPageComponent,
+    StartScreenComponent
   ],
   imports: [
     BrowserModule,
